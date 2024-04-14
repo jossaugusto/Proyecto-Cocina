3ab2f907e87c3680cd51c4e6b1d6c2afafceb65e
Augusto <jpalomino0987@gmail.com>
2024-04-14 01:45:44 -0500

    modificaciones al diseño y agregue iconos al package imagenes

--- Proyecto/src/proyecto/DialogoModificarCocina.java
+++ Proyecto/src/proyecto/DialogoModificarCocina.java
@@ -13,6 +13,13 @@ import javax.swing.JTextField;
 import javax.swing.DefaultComboBoxModel;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.Color;
+import java.awt.Font;
+import javax.swing.border.LineBorder;
+import javax.swing.SwingConstants;
+import java.awt.Cursor;
+import javax.swing.ImageIcon;
+import java.awt.Toolkit;
 
 public class DialogoModificarCocina extends JDialog implements ActionListener {
 
@@ -24,6 +31,7 @@ public class DialogoModificarCocina extends JDialog implements ActionListener {
 	private JTextField txtQuemadores;
 	private JButton btnGrabrar;
 	private JComboBox cboModelo;
+	private JButton btnCerrar;
 
 	/**
 	 * Launch the application.
@@ -42,41 +50,59 @@ public class DialogoModificarCocina extends JDialog implements ActionListener {
 	 * Create the dialog.
 	 */
 	public DialogoModificarCocina() {
+		setBackground(new Color(240, 240, 240));
+		setIconImage(Toolkit.getDefaultToolkit().getImage(DialogoModificarCocina.class.getResource("/Imagenes/iconoEscudo.png")));
+		getContentPane().setBackground(new Color(119, 203, 210));
 		setTitle("Modificar Cocina");
 		setBounds(100, 100, 450, 208);
 		getContentPane().setLayout(null);
 		{
 			JLabel lblModelo = new JLabel("Modelo");
+			lblModelo.setBackground(new Color(255, 255, 255));
+			lblModelo.setForeground(new Color(255, 255, 176));
+			lblModelo.setFont(new Font("Roboto", Font.PLAIN, 13));
 			lblModelo.setBounds(10, 15, 46, 14);
 			getContentPane().add(lblModelo);
 		}
 		{
 			JLabel lblPrecio = new JLabel("Precio (S/)");
+			lblPrecio.setForeground(new Color(255, 255, 176));
+			lblPrecio.setFont(new Font("Roboto", Font.PLAIN, 13));
 			lblPrecio.setBounds(10, 40, 72, 14);
 			getContentPane().add(lblPrecio);
 		}
 		{
 			JLabel lblAncho = new JLabel("Ancho (cm)");
+			lblAncho.setFont(new Font("Roboto", Font.PLAIN, 13));
+			lblAncho.setForeground(new Color(255, 255, 176));
 			lblAncho.setBounds(10, 65, 72, 14);
 			getContentPane().add(lblAncho);
 		}
 		{
 			JLabel lblAlto = new JLabel("Alto (cm)");
+			lblAlto.setForeground(new Color(255, 255, 176));
+			lblAlto.setFont(new Font("Roboto", Font.PLAIN, 13));
 			lblAlto.setBounds(10, 90, 72, 14);
 			getContentPane().add(lblAlto);
 		}
 		{
 			JLabel lblFondo = new JLabel("Fondo (cm)");
+			lblFondo.setFont(new Font("Roboto", Font.PLAIN, 13));
+			lblFondo.setForeground(new Color(255, 255, 176));
 			lblFondo.setBounds(10, 115, 72, 14);
 			getContentPane().add(lblFondo);
 		}
 		{
 			JLabel lblQuemadores = new JLabel("Quemadores");
+			lblQuemadores.setForeground(new Color(255, 255, 176));
+			lblQuemadores.setFont(new Font("Roboto", Font.PLAIN, 13));
 			lblQuemadores.setBounds(10, 140, 81, 14);
 			getContentPane().add(lblQuemadores);
 		}
 		{
 			cboModelo = new JComboBox();
+			cboModelo.setBorder(null);
+			cboModelo.setFont(new Font("Roboto", Font.PLAIN, 12));
 			cboModelo.addActionListener(this);
 			cboModelo.setModel(new DefaultComboBoxModel(new String[] { "Mabe EMP6120PG0", "Indurama Parma", "Sole COSOL027", "Coldex CX602", "Reco Dakota" }));
 			cboModelo.setBounds(92, 11, 118, 22);
@@ -84,47 +110,95 @@ public class DialogoModificarCocina extends JDialog implements ActionListener {
 		}
 		{
 			txtPrecio = new JTextField();
+			txtPrecio.setFont(new Font("Roboto", Font.PLAIN, 12));
+			txtPrecio.setHorizontalAlignment(SwingConstants.CENTER);
+			txtPrecio.setBorder(null);
 			txtPrecio.setBounds(92, 37, 118, 20);
 			getContentPane().add(txtPrecio);
 			txtPrecio.setColumns(10);
 		}
 		{
 			txtAncho = new JTextField();
+			txtAncho.setHorizontalAlignment(SwingConstants.CENTER);
+			txtAncho.setFont(new Font("Roboto", Font.PLAIN, 12));
+			txtAncho.setBorder(null);
 			txtAncho.setBounds(92, 62, 118, 20);
 			getContentPane().add(txtAncho);
 			txtAncho.setColumns(10);
 		}
 		{
 			txtAlto = new JTextField();
+			txtAlto.setHorizontalAlignment(SwingConstants.CENTER);
+			txtAlto.setFont(new Font("Roboto", Font.PLAIN, 12));
+			txtAlto.setBorder(null);
 			txtAlto.setBounds(92, 87, 118, 20);
 			getContentPane().add(txtAlto);
 			txtAlto.setColumns(10);
 		}
 		{
 			txtFondo = new JTextField();
+			txtFondo.setHorizontalAlignment(SwingConstants.CENTER);
+			txtFondo.setFont(new Font("Roboto", Font.PLAIN, 12));
+			txtFondo.setBorder(null);
 			txtFondo.setBounds(92, 112, 118, 20);
 			getContentPane().add(txtFondo);
 			txtFondo.setColumns(10);
 		}
 		{
 			txtQuemadores = new JTextField();
+			txtQuemadores.setHorizontalAlignment(SwingConstants.CENTER);
+			txtQuemadores.setFont(new Font("Roboto", Font.PLAIN, 12));
+			txtQuemadores.setBorder(null);
 			txtQuemadores.setBounds(92, 137, 118, 20);
 			getContentPane().add(txtQuemadores);
 			txtQuemadores.setColumns(10);
 		}
 		{
-			JButton btnCerrar = new JButton("Cerrar");
-			btnCerrar.setBounds(315, 11, 89, 23);
+			btnCerrar = new JButton("");
+			btnCerrar.addActionListener(this);
+			btnCerrar.setContentAreaFilled(false);
+			btnCerrar.setHorizontalAlignment(SwingConstants.LEFT);
+			btnCerrar.setVerticalTextPosition(SwingConstants.BOTTOM);
+			btnCerrar.setHorizontalTextPosition(SwingConstants.CENTER);
+			btnCerrar.setRequestFocusEnabled(false);
+			btnCerrar.setRolloverIcon(new ImageIcon(DialogoModificarCocina.class.getResource("/Imagenes/salir48.png")));
+			btnCerrar.setPressedIcon(new ImageIcon(DialogoModificarCocina.class.getResource("/Imagenes/salir32.png")));
+			btnCerrar.setBorderPainted(false);
+			btnCerrar.setIcon(new ImageIcon(DialogoModificarCocina.class.getResource("/Imagenes/salir32.png")));
+			btnCerrar.setForeground(new Color(255, 255, 255));
+			btnCerrar.setBackground(new Color(255, 255, 255));
+			btnCerrar.setBorder(null);
+			btnCerrar.setOpaque(false);
+			btnCerrar.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+			btnCerrar.setFont(new Font("Roboto", Font.PLAIN, 14));
+			btnCerrar.setBounds(362, 11, 62, 99);
 			getContentPane().add(btnCerrar);
 		}
 
-		btnGrabrar = new JButton("Grabrar");
+		btnGrabrar = new JButton("");
+		btnGrabrar.setHorizontalAlignment(SwingConstants.LEFT);
+		btnGrabrar.setContentAreaFilled(false);
+		btnGrabrar.setRolloverIcon(new ImageIcon(DialogoModificarCocina.class.getResource("/Imagenes/guardar48.png")));
+		btnGrabrar.setPressedIcon(new ImageIcon(DialogoModificarCocina.class.getResource("/Imagenes/guardar32.png")));
+		btnGrabrar.setIcon(new ImageIcon(DialogoModificarCocina.class.getResource("/Imagenes/guardar32.png")));
+		btnGrabrar.setBorderPainted(false);
+		btnGrabrar.setRequestFocusEnabled(false);
+		btnGrabrar.setVerticalTextPosition(SwingConstants.BOTTOM);
+		btnGrabrar.setFont(new Font("Roboto", Font.PLAIN, 14));
+		btnGrabrar.setForeground(new Color(255, 255, 255));
+		btnGrabrar.setBorder(null);
+		btnGrabrar.setBackground(new Color(255, 255, 255));
+		btnGrabrar.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		btnGrabrar.setOpaque(false);
 		btnGrabrar.addActionListener(this);
-		btnGrabrar.setBounds(315, 36, 89, 23);
+		btnGrabrar.setBounds(290, 11, 62, 99);
 		getContentPane().add(btnGrabrar);
 	}
 
 	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == btnCerrar) {
+			actionPerformedBtnCerrar(e);
+		}
 		if (e.getSource() == btnGrabrar) {
 			actionPerformedBtnGrabrar(e);
 		}
@@ -173,5 +247,9 @@ public class DialogoModificarCocina extends JDialog implements ActionListener {
 
 	protected void actionPerformedBtnGrabrar(ActionEvent e) {
 
+	}
+	protected void actionPerformedBtnCerrar(ActionEvent e) {
+	
+		dispose();
 	}
 }// Fin
